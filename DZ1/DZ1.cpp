10a9191fda1e348c73d4e7727cd094d1eac2af6d
ZhapovTB <tizhapov@mail.ru>
2023-09-30 10:38:47 +1000

    Добавьте файлы проекта.

---
+++ DZ1/DZ1.cpp
@@ -0,0 +1,37 @@
+﻿#include <math.h>
+#include <iostream>
+#include <limits>
+float f(float x, int n) {
+    float s1=0, s2=1;
+    for (int k = 1; k <= n; k++)
+    {
+        s1 += 1 / k;
+        s2 *= (k + x) / (k + 1);
+
+    }
+    s1 *= x;
+    return s1 + s2;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float x;
+    int n;
+    std::cout << "Введите x,n" << std::endl;
+    std::cin >> x >> n;
+    while (std::cin.fail())
+    {
+        std::cin.clear();
+        std::cout << "Введите числа x,n верно!" << std::endl;
+        std::cin.ignore(std::numeric_limits < std::streamsize >::max(), '\n');
+        std::cin >> x >> n;
+    }
+   
+   
+    std::cout << f(x, n) << std::endl;
+
+
+    
+}
+
